dff38a24ebe4aa56e37d838be99a81abac01fa98
sion <yinxucun@gmail.com>
2016-04-25 16:21:26 +0800

    Completet  Adapter

--- app/src/main/java/wfu/com/documentexpress/adapter/SettingItemAdapter.java
+++ app/src/main/java/wfu/com/documentexpress/adapter/SettingItemAdapter.java
@@ -1,33 +1,40 @@
 package wfu.com.documentexpress.adapter;
 
 import android.content.Context;
+import android.support.v7.widget.SwitchCompat;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import wfu.com.documentexpress.R;
+
 /**
- * Created by sion on 2016/4/22.
+ * Created by sion on 2016/4/22.、】
+ *
+ * 设置列表的ListView  适配器
  */
 public class SettingItemAdapter extends BaseAdapter {
-    private  int  number;
+
+
+    private LayoutInflater mInflater;
+   private  int  number;
     private  int  ViewId;
     private   Context  context;
     private   ArrayList<String>   arrayList;
-    private static class ViewHolder{
-        CheckBox isCheck;
-        wfu.com.documentexpress.view.RoundRectImageView appIcon;
-        TextView fileName;
-        TextView fileSize;
-        TextView fileModifyTime;
+    public static class ViewHolder{
+        public  TextView  title;
+        public    SwitchCompat  switchCompat;
     }
     public  SettingItemAdapter(Context context,int   ViewId,  ArrayList<String>  arrayList   ){
         this.ViewId=ViewId;
         this.context=context;
+        this.arrayList=arrayList;
+
     }
     @Override
     public int getCount() {
@@ -49,7 +56,20 @@ public class SettingItemAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        View  view=null;
+        ViewHolder  viewHolder;
+        if(convertView==null) {
+            convertView = mInflater.inflate(R.layout.setting_item, null);
+          viewHolder=new ViewHolder();
+            viewHolder.title=(TextView)view.findViewById(R.id.setting_item_text);
+            viewHolder.switchCompat=(SwitchCompat)view.findViewById(R.id.setting_item_button);
+            convertView.setTag(viewHolder);
+        }
+        else{
+              viewHolder=(ViewHolder)convertView.getTag();
 
-        return null;
+        }
+        return convertView;
     }
+
 }
