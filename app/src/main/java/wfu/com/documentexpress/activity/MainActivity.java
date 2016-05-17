e0665769a685cc9985eab4c0accf90657416781f
yinxucun <yinxucun@gmail.com>
2016-05-17 20:22:52 +0800

    完成主活动

--- app/src/main/java/wfu/com/documentexpress/activity/MainActivity.java
+++ app/src/main/java/wfu/com/documentexpress/activity/MainActivity.java
@@ -1,25 +1,27 @@
 package wfu.com.documentexpress.activity;
-import android.app.Application;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.os.SystemClock;
 import android.provider.ContactsContract;
+import android.provider.Settings;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.internal.widget.AdapterViewCompat;
 import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
-import java.io.File;
-import java.io.IOException;
+import android.widget.Toast;
+
 
 import wfu.com.documentexpress.R;
 import wfu.com.documentexpress.utils.SharepreferencesUtilSystemSettings;
@@ -29,13 +31,14 @@ public class MainActivity extends BaseActivity implements CompoundButton.OnCheck
     private  Button  send_file=null;
     private  Button   recieve_file=null;
     private  SwitchCompat setting_sound=null;
-    private  SwitchCompat setting_virbration=null;
+    private  SwitchCompat setting_vibration=null;
     private  TextView  show_dir;
+    private  Button   exit=null;
     public static   boolean  Vibration;
     public  static  boolean  Sound;
     public  static String dirPath;
-    public static   int  LOOD =1 ;
-    public  static    PACKAGE_NAME=
+    public static  String  express_mode;
+    private Spinner  spinner=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -60,29 +63,8 @@ public class MainActivity extends BaseActivity implements CompoundButton.OnCheck
         /*
         * 绘制Materl  Design  的TOOLBar
         * */
-        PackageInfo info = getPackageManager().getPackageInfo(PACKAGE_NAME, 0);
-        int currentVersion = info.versionCode;
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        int lastVersion = prefs.getInt(VERSION_KEY, 0);
-        if (currentVersion > lastVersion) {
-            //如果当前版本大于上次版本，该版本属于第一次启动
-            //将当前版本写入preference中，则下次启动的时候，据此判断，不再为首次启动
-            prefs.edit().putInt(VERSION_KEY,currentVersion).commit();
-        }
         dirPath=SharepreferencesUtilSystemSettings.SETTING;
-         File file=new File(dirPath);
-        if(!file.isFile()){
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        if(LOOD!=1) {
-            SharepreferencesUtilSystemSettings.getValue(getApplicationContext(), "Sound", Sound);
-            SharepreferencesUtilSystemSettings.getValue(getApplicationContext(), "Vibartion", Vibration);
-        }//SharepreferencesUtilSystemSettings  sh=new SharepreferencesUtilSystemSettings(dirPath);
-        //Sound=sh.
+
         Toolbar  toolbar= (Toolbar) findViewById(R.id.toolbar);
         toolbar.setSubtitle(R.string.app_name);
         toolbar.setNavigationIcon(R.drawable.ic_view_headline_black_18dp);
@@ -91,7 +73,7 @@ public class MainActivity extends BaseActivity implements CompoundButton.OnCheck
             @Override
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
-            //hello,word
+                //hello,word
             }
             @Override
             public void onDrawerClosed(View drawerView) {
@@ -102,29 +84,76 @@ public class MainActivity extends BaseActivity implements CompoundButton.OnCheck
         mDrawerLayout.setDrawerListener(mActionBarDrawerToggle);
         send_file=(Button)findViewById(R.id.send_file);
         recieve_file=(Button)findViewById(R.id.recieve_file);
-        send_file.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {//开始发送文件的Activity
-              Intent  intent=new Intent(getApplicationContext(),FileChooseActivity.class);
-              startActivity(intent);
 
-          }
-        });
-    recieve_file.setOnClickListener(new View.OnClickListener() {
-         @Override
-        public void onClick(View v) {  //开始接收文件的Activity
-             Intent  intent=new Intent(getApplicationContext(),FileChooseActivity.class);
-             startActivity(intent);
-
-     }
-    });
         setting_sound=(SwitchCompat)findViewById(R.id.setting_sound);//设置中的声音开关
-        setting_virbration=(SwitchCompat)findViewById(R.id.setting_vibration);//～～～的震动开关
+        setting_vibration=(SwitchCompat)findViewById(R.id.setting_vibration);//～～～的震动开关
         setting_sound.setOnCheckedChangeListener(this);
-        show_dir=(TextView)findViewById(R.id.show_dir);//显示文件默认存储路径的TextView
-       // setting_dir=(Button)findViewById(R.id.button_select_file_dir);
+        setting_vibration.setOnCheckedChangeListener(this);
         LinearLayout  select_dir=(LinearLayout)findViewById(R.id.select_dir);
-        initSwitchButton();
+        exit=(Button)findViewById(R.id.button_exit);
+        show_dir=(TextView)findViewById(R.id.show_dir);//显示文件默认存储路径的TextView
+        spinner=(Spinner)findViewById(R.id.select_mode);
+        SharedPreferences sharedPreferences = this.getSharedPreferences("share", MODE_PRIVATE);
+        boolean isFirstRun = sharedPreferences.getBoolean("isFirstRun", true);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        if (isFirstRun)
+        {
+            Log.d("debug", "第一次运行");
+            editor.putBoolean("isFirstRun", false);
+            editor.commit();
+            SharepreferencesUtilSystemSettings.putValue(this,"Sound",true);
+            SharepreferencesUtilSystemSettings.putValue(this,"vibration",true);
+        } else
+        {
+            Log.d("debug", "不是第一次运行");
+            Sound=SharepreferencesUtilSystemSettings.getValue(getApplicationContext(), "Sound", true);
+            Vibration=SharepreferencesUtilSystemSettings.getValue(getApplicationContext(), "Vibration", true);
+            express_mode=SharepreferencesUtilSystemSettings.getValue(getApplicationContext(),"express_mode","");
+            Log.d("debug",express_mode);
+            if(Sound==true){
+                setting_sound.setChecked(true);
+            }else  setting_sound.setChecked(false);
+            if(Vibration==true)
+                setting_vibration.setChecked(true);
+            else setting_vibration.setChecked(false);
+            Log.d("debug", "setting_sound"+"...");
+            Log.d("debug", "setting_sound"+"0000");
+        }
+        // 建立数据源
+        String[] mItems = getResources().getStringArray(R.array.languages);
+// 建立Adapter并且绑定数据源
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, mItems);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//绑定 Adapter到控件
+        spinner .setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String [] languagess=getResources().getStringArray(R.array.languages);
+                express_mode=languagess[i];
+                Log.d("debug",express_mode);
+                SharepreferencesUtilSystemSettings.putValue(getApplicationContext(),"express_mode",express_mode);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+                Log.d("debug","exit");
+            }
+        });
+
+        // setting_dir=(Button)findViewById(R.id.button_select_file_dir);
+
+        // initSwitchButton();
+
+
         select_dir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {//选择文件的点击监听事件
@@ -134,6 +163,22 @@ public class MainActivity extends BaseActivity implements CompoundButton.OnCheck
                 intent.setType(ContactsContract.Contacts.CONTENT_ITEM_TYPE);
 
                 startActivityForResult(intent, 1);
+
+            }
+        });
+        send_file.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {//开始发送文件的Activity
+                Intent  intent=new Intent(getApplicationContext(),FileChooseActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        recieve_file.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {  //开始接收文件的Activity
+                Intent  intent=new Intent(getApplicationContext(),FileChooseActivity.class);
+                startActivity(intent);
 
             }
         });
@@ -156,51 +201,36 @@ public class MainActivity extends BaseActivity implements CompoundButton.OnCheck
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         switch (buttonView.getId()) {
-            case R.id.setting_sound:
-                Log.d("Tag","setting_sound");
+            case R.id.setting_sound: {
+                Log.d("debug", "setting_sound");
                 if (setting_sound.isChecked()) {
-
-                    Log.d("Tag","setting_sound_ischeched");
-                    Sound=true;
-                    SharepreferencesUtilSystemSettings.putValue(getApplicationContext(),"Sound",true);
-                }
-                else  {
-                    Sound=false;
-                    Log.d("Tag","setting_sound_unischeked");
-                    SharepreferencesUtilSystemSettings.putValue(getApplicationContext(),"Sound",false);
+                    Sound = true;
+                    SharepreferencesUtilSystemSettings.putValue(getApplicationContext(), "Sound", true);
+                    Log.d("debug", "setting_sound"+Sound);
+                } else {
+                    Sound = false;
+                    SharepreferencesUtilSystemSettings.putValue(getApplicationContext(), "Sound", false);
+                    Log.d("debug", "setting_sound"+Sound);
                 }
                 break;
-
-            case R.id.setting_vibration:
-                if (setting_virbration.isChecked()) {
-                    Vibration=true;
-                    SharepreferencesUtilSystemSettings.putValue(getApplicationContext(),"Vibration",true);
-
+            }
+            case R.id.setting_vibration: {
+                Log.d("debug", "setting_sound");
+                if (setting_vibration.isChecked()) {
+                    Vibration = true;
+                    SharepreferencesUtilSystemSettings.putValue(getApplicationContext(), "Vibration", true);
+                    Log.d("debug", "setting_sound"+Vibration);
+                } else {
+                    Vibration = false;
+                    SharepreferencesUtilSystemSettings.putValue(getApplicationContext(), "Vibration", false);
+                    Log.d("debug", "setting_sound"+Vibration);
                 }
-                else {
-                    Vibration=false;
-                    SharepreferencesUtilSystemSettings.putValue(getApplicationContext(),"Vibration",false);
-                }
+            }
                 break;
+            default:break;
         }
     }
 
 
-    private   void initSwitchButton  (){
-        if  (Sound==false){
-            setting_sound.setChecked(false);
-            Log.d("Tag","setting  setting_sound");
-        }
-        else  setting_sound.setClickable(true);
-        if(Vibration==false){
-            setting_virbration.setClickable(false);
-        }
-        else  setting_virbration.setChecked(true);
-    }
-    @Override
-    protected   void onDestroy(){
-        LOOD=2;
 
-
-    }
 }
---
+++ app/src/main/java/wfu/com/documentexpress/activity/NetWorkExpress.java
@@ -0,0 +1,16 @@
+package wfu.com.documentexpress.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by yinxucun on 16-5-17.
+ */
+public class NetWorkExpress extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
